6c612ccfd2cb54038f98976412a157a6524c996e
Jusley Souza <jusleysouza31@gmail.com>
2022-12-08 14:59:28 -0300

    Project test class

---
+++ src/test/java/com/ju/springboot/rsocket/RsocketpatientserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ju.springboot.rsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsocketpatientserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
