e8398e48f6b69eb167fb3ae61d1a931125050f02
Jusley Souza <jusleysouza31@gmail.com>
2022-12-08 15:31:47 -0300

    Adding logger

--- src/main/java/com/ju/springboot/rsocket/controllers/RSocketPatientController.java
+++ src/main/java/com/ju/springboot/rsocket/controllers/RSocketPatientController.java
@@ -1,5 +1,8 @@
 package com.ju.springboot.rsocket.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 
 import com.ju.springboot.rsocket.model.ClinicalData;
@@ -9,8 +12,12 @@ import reactor.core.publisher.Mono;
 
 @Controller
 public class RSocketPatientController {
+	
+	Logger logger = LoggerFactory.getLogger(RSocketPatientController.class);
 
+	@MessageMapping("get-patient-data")
 	public Mono<ClinicalData> requestResponse(Patient patient){
+		logger.info("Received Patient: " + patient);
 		return Mono.just(new ClinicalData(90, "80/120"));
 		
 	}
