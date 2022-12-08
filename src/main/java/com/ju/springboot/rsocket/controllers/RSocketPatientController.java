e26f10d7b4b3ae23aa61beba563210663299f8b5
Jusley Souza <jusleysouza31@gmail.com>
2022-12-08 15:46:03 -0300

    Refactoring code

--- src/main/java/com/ju/springboot/rsocket/controllers/RSocketPatientController.java
+++ src/main/java/com/ju/springboot/rsocket/controllers/RSocketPatientController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.ju.springboot.rsocket.model.ClinicalData;
 import com.ju.springboot.rsocket.model.Patient;
@@ -16,7 +17,7 @@ public class RSocketPatientController {
 	Logger logger = LoggerFactory.getLogger(RSocketPatientController.class);
 
 	@MessageMapping("get-patient-data")
-	public Mono<ClinicalData> requestResponse(Patient patient){
+	public Mono<ClinicalData> requestResponse(@RequestBody Patient patient){
 		logger.info("Received Patient: " + patient);
 		return Mono.just(new ClinicalData(90, "80/120"));
 		
